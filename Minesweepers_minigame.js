54317ff9c492986fedcf14e40d5bde3ec7daf790
Fabian <wyssfabian1@gmail.com>
2026-07-23 20:55:26 +0200

    added win & lose screen with button to reset.

--- Minesweepers_minigame.js
+++ Minesweepers_minigame.js
@@ -1,4 +1,7 @@
 const grid = document.getElementById("grid");
+const overlayLose = document.getElementById("overlayLose");
+const overlayWin = document.getElementById("overlayWin");
+const button = document.getElementById("button");
 const directions = [
     [-1, -1], [-1, 0], [-1, 1],
     [0, -1], [0, 1],
@@ -6,46 +9,52 @@ const directions = [
 ]
 let rows = 10;
 let colums = 10;
-let MineCount = 20;
-
-let data = new Array();
-
 grid.style.gridTemplateRows = `repeat(${rows},1fr)`;
 grid.style.gridTemplateColumns = `repeat(${colums},1fr)`;
 
-for(let i=0; i<colums; i++){
-    for(let j=0; j<rows; j++){
-        let item = document.createElement('div');
-        item.textContent=`${i} ${j}`;
-        item.id = `${i} ${j}`;
-        grid.appendChild(item);
+let MineCount = 20;
+let AmountOfFieldsUncovered = 0;
+let data = new Array();
+
+function reset(){
+    grid.textContent = '';
+    AmountOfFieldsUncovered = 0;
+    data = new Array();
+
+
+    for(let i=0; i<colums; i++){
+        for(let j=0; j<rows; j++){
+            let item = document.createElement('div');
+            item.textContent="O"; //`${i} ${j}`
+            item.id = `${i} ${j}`;
+            grid.appendChild(item);
+        }
+        data.push(new Array(rows).fill(0));
     }
-    data.push(new Array(rows).fill(0));
-}
-for(let i=0; i<MineCount; i++){
-    let x = Math.floor(Math.random()*colums);
-    let y = Math.floor(Math.random()*rows);
+    for(let i=0; i<MineCount; i++){
+        let x = Math.floor(Math.random()*colums);
+        let y = Math.floor(Math.random()*rows);
 
-    data[x][y]=-1;
-}
-for(let i=0; i<colums; i++){
-    for(let j=0; j<rows; j++){
-        if(data[i][j]==-1){
-            for(let q=0; q<8; q++){
-                let dx = directions[q][0];
-                let dy = directions[q][1]
-                let x = i+dx;
-                let y = j+dy;
-                //console.log(x,y);
-                if(0 <= x && x < colums && 0 <= y && y < rows){
-                    if(data[x][y]!=-1) data[x][y]+=1;
+        data[x][y]=-1;
+    }
+    for(let i=0; i<colums; i++){
+        for(let j=0; j<rows; j++){
+            if(data[i][j]==-1){
+                for(let q=0; q<8; q++){
+                    let dx = directions[q][0];
+                    let dy = directions[q][1]
+                    let x = i+dx;
+                    let y = j+dy;
+                    //console.log(x,y);
+                    if(0 <= x && x < colums && 0 <= y && y < rows){
+                        if(data[x][y]!=-1) data[x][y]+=1;
+                    }
                 }
-
             }
-            
         }
     }
 }
+reset();
 
 console.log(data);
 
@@ -61,41 +70,61 @@ function BFS(startx, starty, target){
         let y = queue[0][1];
         queue.shift();
         for(let q=0; q<8; q++){
-                let dx = directions[q][0];
-                let dy = directions[q][1]
-                let x2 = x+dx;
-                let y2 = y+dy;
-                //console.log(x,y);
-                if(0 <= x2 && x2 < colums && 0 <= y2 && y2 < rows){
-                    if(vis[x2][y2]==0 && data[x2][y2]==target){
-                        queue.push([x2,y2]);
-                        vis[x2][y2]=1;
+            let dx = directions[q][0];
+            let dy = directions[q][1]
+            let x2 = x+dx;
+            let y2 = y+dy;
+            //console.log(x,y);
+            if(0 <= x2 && x2 < colums && 0 <= y2 && y2 < rows){
+                if(vis[x2][y2]==0){
+                    vis[x2][y2]=1;
+                    
+                    let obj = document.getElementById(`${x2} ${y2}`);
+                    obj.textContent = data[x2][y2];
+                    obj.style.backgroundColor = 'green';
 
-                        let obj = document.getElementById(`${x2} ${y2}`);
-                        obj.textContent = data[x][y];
-                        obj.style.backgroundColor = 'green';
+                    if(data[x2][y2]==target){
+                        queue.push([x2,y2]);
                     }
                 }
+            }
         }
-        
     }
-    
-
 }
 
 
+
 grid.addEventListener('click', function(event) {
+    const mode = document.getElementById("mode").checked;
+
     let x = Number(event.target.id.split(" ")[0]);
     let y = Number(event.target.id.split(" ")[1]);
-    console.log(x,y,event.target.id);
-    event.target.textContent = data[x][y];
-    if(data[x][y]==-1){
-        event.target.style.backgroundColor = 'red';
+    //console.log(x,y,event.target.id);
+    if(mode){
+        if(event.target.textContent == "X"){
+            event.target.textContent = "O";
+            event.target.style.backgroundColor = '#f1f1f1';
+        } else{
+            event.target.textContent = "X";
+            event.target.style.backgroundColor = 'orange';
+        }
     } else{
-        event.target.style.backgroundColor = 'green';
-        if(data[x][y]==0){
-            BFS(x,y,0);
+        event.target.textContent = data[x][y];
+        if(data[x][y]==-1){
+            event.target.style.backgroundColor = 'red';
+            overlayLose.style.display = 'block';
+        } else{
+            event.target.style.backgroundColor = 'green';
+            if(data[x][y]==0){
+                BFS(x,y,0);
+            }
         }
     }
-    
+    if(AmountOfFieldsUncovered == colums*rows-MineCount){
+        overlayWin.style.display = 'block';
+    }
+})
+button.addEventListener('click', () => {
+    reset();
+    overlayLose.style.display = 'none';
 })
